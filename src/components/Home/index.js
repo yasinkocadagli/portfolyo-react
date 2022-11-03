e94f93e09ffdc20b3d79c9e66c346f7983311e77
yasin <yasinkocadagli@gmail.com>
2022-11-03 17:50:31 +0300

    first commit

---
+++ src/components/Home/index.js
@@ -0,0 +1,28 @@
+import LogoTitle from '../../assets/images/logo-s.png';
+import {Link} from 'react-router-dom';
+import './index.scss';
+
+const Home=()=>{
+    
+    
+
+    return(
+        <div className='container home-page'>
+           <div className='text-zone'>
+              <h1>Hi, <br /> I'm
+              <img src={LogoTitle} alt='developer' />
+              asin
+              <br />
+              Web Developer
+              </h1>
+              <h2>Frontend Developer /Learning JS / Perfect human</h2>
+              <Link to="/contact" className='flat-button'> CONTACT ME</Link>
+
+           </div>
+
+        </div>
+    )
+}
+
+
+export default Home
