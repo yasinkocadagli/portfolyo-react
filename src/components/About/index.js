686e756c6bc8fa740fd24201a037773f385fc365
yasin <yasinkocadagli@gmail.com>
2022-11-07 14:36:17 +0300

    updated about-page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,4 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import AnimatedLetters from '../AnimatedLetters'
-import './index.scss'
+import { useEffect, useState } from 'react'
 import {
   faAngular,
   faCss3,
@@ -10,33 +8,41 @@ import {
   faReact,
 } from '@fortawesome/free-brands-svg-icons'
 import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './index.scss'
 
 const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+//   useEffect(() => {
+//     return setTimeout(() => {
+//       setLetterClass('text-animate-hover')
+//     }, 3000)
+//   }, [])
+
   return (
     <>
       <div className="container about-page">
         <div className="text-zone">
           <h1>
             <AnimatedLetters
+              letterClass={letterClass}
               strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
               idx={15}
             />
           </h1>
           <p>
-            I'm a very ambitious front-end developer looking for a role in an
-            established IT company with the opportunity to work with the latest
-            technologies on challenging and diverse projects.
+            Muğla Sıtkı Koçman Üniversitesi bilişim sistemleri mühendisiğliği 2.sınıf öğrencisiyim
           </p>
           <p align="LEFT">
-            I'm quiet confident, naturally curious, and perpetually working on
-            improving my chops one design problem at a time.
+           Reactta kendimi geliştirmek için projeler yapıyorum bu da onlardan bi tanesi. 
           </p>
           <p>
-            If I need to define myself in one sentence that would be a family
-            person, father of a beautiful daughter, a sports fanatic,
-            photography enthusiast, and tech-obsessed!!!
+            Buraya daha sonra farklı şeyler yazıcam ama doldurmak için şimdilik böyke kalsın ana sayfaya gidebilirsin teşekkürler.
           </p>
         </div>
+
         <div className="stage-cube-cont">
           <div className="cubespinner">
             <div className="face1">
@@ -60,7 +66,7 @@ const About = () => {
           </div>
         </div>
       </div>
-      <Loader type='pacman' />
+      <Loader type="pacman" />
     </>
   )
 }
